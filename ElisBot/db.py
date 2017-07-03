3abe5246f8e5843f045f9de69e3d6bb5ee522a78
iflare3g <iflare3g@gmail.com>
2017-07-03 17:32:16 +0000

    no credentials visible

--- ElisBot/db.py
+++ ElisBot/db.py
@@ -8,7 +8,7 @@ class Connection:
     #Tentativo di connessione al DB
     def connect(self):
         try:
-            self.connection = mariadb.connect(user='root', password='elisbot', db='Residence_DB', host='127.0.0.1',cursorclass=pymysql.cursors.DictCursor)
+            self.connection = mariadb.connect(user='YOUR-USER', password='YOUR-PWD', db='YOUR-DB', host='YOUR-HOST',cursorclass=pymysql.cursors.DictCursor)
 
         except Exception as e:
             print('Connection failed! %s,' % e)
